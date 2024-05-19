1a5015dd045076e2d1811a5b74f63c4d20d37675
Karanjkar Nilesh <44108667+Karanjkarnilesh@users.noreply.github.com>
2024-05-19 23:38:43 +0530

    add react first class

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+ReactDom.render(<App/>,document.getElementById('root'));
 
-console.log("Helllo");
